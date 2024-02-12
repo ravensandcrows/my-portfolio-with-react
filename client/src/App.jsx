a48a4e9627a3035aa8398fd2f3a88c24ffe82743
ravensandcrows <blackstormkali@gmail.com>
2024-02-11 21:08:46 -0800

    added footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero/Hero';
 import About from './components/About/About';
 import Portfolio from './components/Portfolio/Portfolio';
 import ContactInfo from './components/ContactInfo/ContactInfo';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <About />
       <Portfolio />
       <ContactInfo />
+      <Footer />
     </div>
   )
 }
---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer>
+            <h3>Made with Love ©2023</h3>
+        </footer>
+    )
+}
+
+export default Footer
