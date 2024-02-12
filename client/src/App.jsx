c7e7a9863ab4acb993677f298f01ee722306181f
ravensandcrows <blackstormkali@gmail.com>
2024-02-11 18:30:09 -0800

    added the about section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
+import About from './components/About/About';
 
 function App() {
   return (
     <div>
       <Header />
       <Hero />
+      <About />
     </div>
   )
 }
