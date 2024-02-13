b5633cc8048df32902e7278ac394a71b4b429d74
ravensandcrows <blackstormkali@gmail.com>
2024-02-12 22:40:59 -0800

    added the icon images for the contactinfo

--- client/src/components/ContactInfo/index.jsx
+++ client/src/components/ContactInfo/index.jsx
@@ -1,11 +1,24 @@
-function ContactInfo () {
-    return (
-        <section id="contact">
-        <ul>
-            <li className="card-contact">GITHUB</li>
-            <li className="card-contact">LINKEDIN</li>
-        </ul>
+function ContactInfo() {
 
+    const icons = [
+        {
+            name: "fab fa-github",
+            link: "https://github.com/ravensandcrows"
+        },
+        {
+            name: "fab fa-linkedin",
+            link: "https://www.linkedin.com/in/kaliblackstorm"
+        }
+    ]
+    return (
+        <section className="contactInfoBC">
+            {icons.map(icon =>
+            (
+                <a href={icon.link} key={icon.name} target="_blank" rel="noopener noreferrer" className="anchor">
+                    <i className={icon.name}></i>
+                </a>
+            )
+            )}
         </section>
     )
 }
