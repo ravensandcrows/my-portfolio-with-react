b557fc5060cceb8d9cd9aeba7b869c0df30d169c
ravensandcrows <blackstormkali@gmail.com>
2024-06-19 15:21:24 -0700

    updated resume page

--- src/components/Page/index.jsx
+++ src/components/Page/index.jsx
@@ -6,14 +6,39 @@ import { Outlet } from 'react-router-dom';
 
 function Page({ currentPage }) {
     currentPage = currentPage.substring(1);
-
     useEffect(() => {
         document.title = capitalizeFirstLetter(currentPage);
     }, [currentPage]);
 
+    const scrollToElement = (elementId) => {
+        const element = document.getElementById(elementId);
+        if (element) {
+          element.scrollIntoView({ behavior: "smooth" });
+        }
+      };
+
     return (
         <section>
-            <h2>{capitalizeFirstLetter(currentPage)}</h2>
+            {currentPage === 'resume' ? (
+                <div className='resume-header'>
+                    <h2>{capitalizeFirstLetter(currentPage)}</h2>
+                    <nav className='resume-nav'>
+                        <ul className='resume-ul'>
+                            <li className='resume-li'>
+                                <button onClick={()=>scrollToElement('front-end')} className='resume-button'>Front-End</button>
+                            </li>
+                            <li className='resume-li'>
+                                <button onClick={()=>scrollToElement('back-end')} className='resume-button'>Back-End</button>
+                            </li>
+                            <li className='resume-li'>
+                                <a className="resume-button" target="_blank" href="https://www.linkedin.com/in/kaliblackstorm">LinkedIn</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            ) : (
+                <h2>{capitalizeFirstLetter(currentPage)}</h2>
+            )}
             <PageContent>
                 <Outlet />
             </PageContent>
